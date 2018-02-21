fe3a4ee587b60b1ad64a6252993cd8584c948034
François Michaud <fmichaud@9fevrier.com>
2018-02-21 18:13:14 +0100

    ...

---
+++ test/route-http.test.js
@@ -0,0 +1,9 @@
+/**
+ * This file is part of pigalle.routes.http
+ *
+ * Copyright (c) 2018 SAS 9 Février.
+ *
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ *
+ */
+
